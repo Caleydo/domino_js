6b0038ebaef0f3440409e67e582903898fa000f9
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-12-15 11:30:57 +0100

    add selection + simple add of elements

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 /**
  * Created by Samuel Gratzl on 15.12.2014.
  */
-require(['../caleydo/data', '../caleydo/vis', 'bootstrap'], function (data, visPlugins) {
+require(['../caleydo/data', 'd3', '../caleydo/event', '../caleydo-selectioninfo/main', '../caleydo-window/main', '../caleydo/vis', 'bootstrap'], function (data, d3, events, selectionInfo, windows, vis) {
   'use strict';
-  var vis;
+  selectionInfo.create(document.getElementById('selectioninfo'));
+  var content = document.getElementById('board');
 
-  data.create({
-    type: 'genomeDataLink',
-    name: 'GenomeBrowser',
-    serveradress: 'http://localhost:5000/bam'
-  }).then(function (genome) {
-    var visses = visPlugins.list(genome);
-
-    visses[0].load().then(function (plugin) {
-      vis = plugin.factory(genome, document.getElementsByName('body')[0]);
+  data.list().then(function (items) {
+    var $base = d3.select('#blockbrowser table tbody');
+    var $rows = $base.selectAll('tr').data(items);
+    $rows.enter().append('tr').html(function (d) {
+      return '<th>' + d.desc.name + '</th><td>' + d.desc.type + '</td><td>' + d.dim.join(' x ') + '</td>';
+    }).on('click', function (dataset) {
+      var visses = vis.list(dataset);
+      var w = windows.createVisWindow(content);
+      visses[0].load().then(function (plugin) {
+        w.attachVis(plugin.factory(dataset, w.node), plugin);
+      });
     });
   });
 });
