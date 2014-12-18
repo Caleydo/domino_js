6594ed47e87d33ea0f5d235c578c09071c257c10
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-12-18 11:59:02 +0100

    add dummy links wrapper

---
+++ links.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Samuel Gratzl on 16.12.2014.
+ */
+define(['exports', 'd3', './block', ''], function (exports, d3, blocks) {
+  "use strict";
+  exports.createLayer = function (parent) {
+    var $div = d3.select(parent).append('div');
+
+    function update() {
+
+    }
+
+    function added() {
+
+    }
+
+    function removed() {
+
+    }
+
+    blocks.manager.on('add', added);
+    blocks.manager.on('remove', removed);
+    blocks.manager.on('select-selected', update);
+
+    return $div;
+  };
+});
