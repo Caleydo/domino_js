002eb45074a476dcc044c3bf0599b1120a620488
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-03 10:47:56 +0100

    phovea_bootstrap_fontawesome to phovea_ui

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@
 import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
 import 'file-loader?name=404.html!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
-import 'phovea_bootstrap_fontawesome/src/_font-awesome';
-import 'phovea_bootstrap_fontawesome/src/_bootstrap';
+import 'phovea_ui/src/_font-awesome';
+import 'phovea_ui/src/_bootstrap';
 import './style.scss';
 
 import * as domino from './Domino';
