f2732cd1df0eee9af1c5348c9611e4bdcc3f4750
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-14 10:00:59 +0100

    get rid of JQuery

--- src/Board.ts
+++ src/Board.ts
@@ -3,70 +3,38 @@
 import {select, event as d3event} from 'd3';
 import {ObjectManager, clearSelection} from 'phovea_core/src/idtype';
 import {LinkContainer} from 'phovea_d3/src/link';
-import {Block, createBlockAt} from './Block';
-import {BlockDecorator} from './BlockDecorator';
+import {Block, createBlockAt, IDominoDataType} from './Block';
 import {hasDnDType} from 'phovea_core/src';
 import {get as getData} from 'phovea_core/src/data';
 
 export class Board {
-  private content:Element;
-  private links:LinkContainer;
-  public $node;
+  private content: HTMLElement;
+  private links: LinkContainer;
+  $node: d3.Selection<any>;
 
-  private blocks : {
-    currentlyDragged; //untyped because of generic block definition
-    manager:ObjectManager<Block<BlockDecorator>>;
+  private blocks: {
+    currentlyDragged: Block;
+    manager: ObjectManager<Block>;
   };
 
-  constructor(node:Element) {
+  constructor(node: HTMLElement) {
     const that = this;
     this.content = node;
-    this.links = new LinkContainer(node, ['change', 'transform', 'change.pos', 'change.range', 'zoom'],
-      {
-        filter: (a: Block<BlockDecorator>, b: Block<BlockDecorator>) => {
-          console.log('check block occlusion');
-          const retval = {val: true};
-
-          this.blocks.manager.forEach(function (block) {
-            const a = this[0];
-            const b = this[1];
-            const retval = this[2];
-
-            if (!retval.val) {
-              return;
-            }
-
-            if (block.id !== a.id && block.id !== b.id) {
-              let leftelempos = b.$node[0].offsetLeft;
-              let rightelempos = a.$node[0].offsetLeft;
-              if (a.$node[0].offsetLeft < b.$node[0].offsetLeft) {
-                leftelempos = a.$node[0].offsetLeft;
-                rightelempos = b.$node[0].offsetLeft;
-              }
-
-              if (leftelempos < block.$node[0].offsetLeft && block.$node[0].offsetLeft < rightelempos) {
-                retval.val = false;
-              }
-            }
-
-          }, [a, b, retval]);
-          return retval.val;
-        }
+    this.links = new LinkContainer(node, ['change', 'transform', 'change.pos', 'change.range', 'zoom'], {
+        filter: this.checkBlockCollisions.bind(this)
       });
 
     this.blocks = {
       currentlyDragged: null,
-      manager: new ObjectManager<Block<BlockDecorator>>('block', 'Block')
+      manager: new ObjectManager<Block>('block', 'Block')
     };
 
     this.blocks.manager.on('select', function (event, type) {
       that.blocks.manager.forEach(function (block) {
-        block.decorator.$header.classed('phovea-select-' + type, false);
-        block.$node.removeClass('phovea-select-' + type);
+        block.node.classList.remove('phovea-select-' + type);
       });
       that.blocks.manager.selectedObjects(type).forEach(function (block) {
-        block.decorator.$header.classed('phovea-select-' + type, true);
-        block.$node.addClass('phovea-select-' + type);
+        block.node.classList.add('phovea-select-' + type);
       });
     });
     this.blocks.manager.on('add', function (event, id, block) {
@@ -84,26 +52,12 @@ export class Board {
     });
 
     //dnd operation
-    this.$node.on({
-      'drop': () => {
-        this.drop();
-      },
-      'dragenter': () => {
-        this.dragEnter();
-      },
-      'dragover': () => {
-        this.dragOver();
-      },
-      'dragleave': () => {
-        this.dragLeave();
-      },
-      'mousemove': () => {
-        this.mouseMove();
-      },
-      'mouseup': () => {
-        this.mouseUp();
-      }
-    });
+    this.$node.on('drop', () => this.drop())
+      .on('dragenter', () => this.dragEnter())
+      .on('dragover', () => this.dragOver())
+      .on('dragleave', () => this.dragLeave())
+      .on('mousemove', () => this.mouseMove())
+      .on('mouseup', () => this.mouseUp());
   }
 
   public set currentlyDragged(block) {
@@ -115,27 +69,26 @@ export class Board {
   }
 
   private mouseUp() {
-    if(null !== this.blocks.currentlyDragged) {
+    if (null !== this.blocks.currentlyDragged) {
       this.blocks.currentlyDragged.dragging = false;
 
     }
   }
 
   private mouseMove() {
-    if(null !== this.blocks.currentlyDragged) {
+    if (null !== this.blocks.currentlyDragged) {
       const e = <MouseEvent> d3event;
       const coords = [e.offsetX, e.offsetY];
       const blockOffset = this.blocks.currentlyDragged.dragOffset;
-      if(false !== blockOffset) {
+      if (false !== blockOffset) {
         coords[0] -= blockOffset[0];
         coords[1] -= blockOffset[1];
       }
-      this.blocks.currentlyDragged.pos = coords;
+      this.blocks.currentlyDragged.pos = <[number,number]>coords;
     }
   }
 
   private dragEnter() {
-    console.log('dragEnter');
     const e = <DragEvent> d3event;
     if (hasDnDType(e, 'application/phovea-data-item') || hasDnDType(e, 'application/phovea-domino-dndinfo')) {
       return false;
@@ -143,7 +96,6 @@ export class Board {
   }
 
   private dragOver() {
-    console.log('dragOver');
     const e = <DragEvent> d3event;
     this.links.update();
     if (hasDnDType(e, 'application/phovea-data-item') || hasDnDType(e, 'application/phovea-domino-dndinfo')) {
@@ -153,11 +105,10 @@ export class Board {
   }
 
   private dragLeave() {
-    console.log('dragLeave');
+    //
   }
 
   private drop() {
-    console.log('drop');
     const e = <DragEvent> d3event;
     e.preventDefault();
     //data move
@@ -166,20 +117,51 @@ export class Board {
       const that = this;
       getData(id).then((d) => {
         //CLUE CMD
-        createBlockAt(d, that.content, that, [e.offsetX, e.offsetY], this.blocks.manager);
+        createBlockAt(<IDominoDataType>d, that.content, that, [e.offsetX, e.offsetY], this.blocks.manager);
       });
       this.currentlyDragged = null;
       return false;
     }
   }
 
-  digestKeyCode(e:KeyboardEvent) {
+  private checkBlockCollisions(a: Block, b: Block) {
+    let hasCollision = false;
+
+    this.blocks.manager.forEach((block) => {
+      if (hasCollision) {
+        return;
+      }
+
+      if (block.id !== a.id && block.id !== b.id) {
+        let leftelempos = b.node.offsetLeft;
+        let rightelempos = a.node.offsetLeft;
+        if (a.node.offsetLeft < b.node.offsetLeft) {
+          leftelempos = a.node.offsetLeft;
+          rightelempos = b.node.offsetLeft;
+        }
+
+        if (leftelempos < block.node.offsetLeft && block.node.offsetLeft < rightelempos) {
+          hasCollision = true;
+        }
+      }
+
+    });
+    return !hasCollision;
+  }
+
+  digestKeyCode(e: KeyboardEvent) {
+    const selectedBlocks = this.blocks.manager.selections();
+    if (selectedBlocks.isNone) {
+      return;
+    }
+
+
     e.preventDefault(); // prevent the default action (scroll / move caret)
-    const dxy:[number,number] = [0,0];
+    const dxy: [number, number] = [0, 0];
 
     const amount = e.ctrlKey ? 15 : 5;
 
-    switch(e.which) {
+    switch (e.which) {
       case 37: // left
         dxy[0] -= amount;
         break;
@@ -196,7 +178,6 @@ export class Board {
         return; // exit this handler for other keys
     }
 
-    const selectedBlocks = this.blocks.manager.selections();
     selectedBlocks.dim(0).forEach((id) => {
       this.blocks.manager.byId(id).moveBy(dxy[0], dxy[1]);
     });
